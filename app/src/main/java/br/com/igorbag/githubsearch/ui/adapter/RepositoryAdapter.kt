e2577a4ce63552a36b6486f50f56ac4a40df972a
Gigliárly Marcelino <60317202+GigliarlyM@users.noreply.github.com>
2023-09-17 19:06:29 -0300

    ConclusaoAPP
    Finalizando o aplicativo do desafio

--- app/src/main/java/br/com/igorbag/githubsearch/ui/adapter/RepositoryAdapter.kt
+++ app/src/main/java/br/com/igorbag/githubsearch/ui/adapter/RepositoryAdapter.kt
@@ -1,17 +1,33 @@
 package br.com.igorbag.githubsearch.ui.adapter
 
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
 import br.com.igorbag.githubsearch.R
 import br.com.igorbag.githubsearch.domain.Repository
 
-class RepositoryAdapter(private val repositories: List<Repository>) :
+class RepositoryAdapter(
+    private val context: Context,
+    private val repositories: List<Repository>) :
     RecyclerView.Adapter<RepositoryAdapter.ViewHolder>() {
 
-    var carItemLister: (Repository) -> Unit = {}
-    var btnShareLister: (Repository) -> Unit = {}
+    var carItemLister: (Repository) -> Unit = {
+        Log.d("clickItem", it.htmlUrl)
+        openBrowser(context, it.htmlUrl)
+    }
+    var btnShareLister: (Repository) -> Unit = {
+        Log.d("click", it.htmlUrl)
+        shareRepositoryLink(context, it.htmlUrl)
+    }
 
     // Cria uma nova view
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -22,36 +38,55 @@ class RepositoryAdapter(private val repositories: List<Repository>) :
 
     // Pega o conteudo da view e troca pela informacao de item de uma lista
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        //@TODO 8 -  Realizar o bind do viewHolder
-        //Exemplo de Bind
-        //  holder.preco.text = repositories[position].atributo
+        holder.nomeRepo.text = repositories[position].name
 
-        // Exemplo de click no item
-        //holder.itemView.setOnClickListener {
-        // carItemLister(repositores[position])
-        //}
+        holder.cvRepo.setOnClickListener{
+            carItemLister(repositories[position])
+        }
 
-        // Exemplo de click no btn Share
-        //holder.favorito.setOnClickListener {
-        //    btnShareLister(repositores[position])
-        //}
+        holder.ivShare.setOnClickListener {
+            btnShareLister(repositories[position])
+        }
     }
 
-    // Pega a quantidade de repositorios da lista
-    //@TODO 9 - realizar a contagem da lista
-    override fun getItemCount(): Int = 0
+    fun shareRepositoryLink(context: Context, urlRepository: String) {
+        val sendIntent: Intent = Intent().apply {
+            action = Intent.ACTION_SEND
+            putExtra(Intent.EXTRA_TEXT, urlRepository)
+            type = "text/plain"
+        }
+
+        val shareIntent = Intent.createChooser(sendIntent, null)
+        shareIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        context.startActivity(shareIntent)
+    }
+
+    fun openBrowser(context: Context, urlRepository: String) {
+        context.startActivity(
+            Intent(
+                Intent.ACTION_VIEW,
+                Uri.parse(urlRepository)
+            ).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        )
+    }
+
+    override fun getItemCount(): Int = repositories.size
 
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        //@TODO 10 - Implementar o ViewHolder para os repositorios
-        //Exemplo:
-        //val atributo: TextView
+        val nomeRepo: TextView
+        val ivShare: ImageView
+        val cvRepo: CardView
 
-        //init {
-        //    view.apply {
-        //        atributo = findViewById(R.id.item_view)
-        //    }
+        init {
+            view.apply {
+                nomeRepo = findViewById(R.id.tv_name_repo)
+                ivShare = findViewById(R.id.iv_share)
+                cvRepo = findViewById(R.id.cv_repo)
+            }
+        }
 
     }
 }
+
 
 
